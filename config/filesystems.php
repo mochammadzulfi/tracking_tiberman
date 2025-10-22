79fb140e57b08cb1d75f167f9d745625e89bf790
zulfi <m.zulfi@duakelinci.co.id>
2025-10-22 11:07:25 +0700

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,8 +40,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => base_path('../public_html/storage'), // path ke folder storage di public_html
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
